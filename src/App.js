4965163f3cbe5f4d9c856cb5961fa34e81459809
Faisal Iqbal <7faisaliqbal@gmail.com>
2022-09-03 11:41:25 +0500

    style: created start now section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import { Footer, NavBar } from "./components";
+import { Footer, NavBar, StartNow } from "./components";
 
 function App() {
   return (
     <div className="root">
       <NavBar />
+      <StartNow />
       <Footer />
     </div>
   );
---
+++ src/components/StartNow/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./StartNow.css";
+
+export default function StartNow() {
+  return (
+    <div class="start-now">
+      <h1 class="title">Boost your links today</h1>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  );
+}
